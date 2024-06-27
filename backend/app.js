b765a7cfe164102adae62711c743c788e69061d5
PrachponleuUch <ponleuvuthy@gmail.com>
2024-06-27 15:20:39 +1000

    Setup a test route

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@ const app = express() // For register routes and listen to ports
 import dotenv from 'dotenv'
 dotenv.config({path: 'backend/config/config.env'})
 
+import productRoutes from './routes/products.js'
+app.use("/api/v1", productRoutes)
+
 app.listen((process.env.PORT), ()=> {
   console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV}mode.`)
 })
