feeeb41f9b874c9fdee0f7ef5c8ad720dc1daa89
PrachponleuUch <ponleuvuthy@gmail.com>
2024-06-28 14:06:29 +1000

    Added GET, PUT, DELETE BY ID route for product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -10,9 +10,58 @@ export const getProducts = async (req, res) => {
 
 // Create new product ==> /api/v1/admin/products
 export const newProduct = async(req, res) => {
-  const product = await Product.create(req.body)
+  const product = await Product.create(req?.body)
 
   res.status(200).json({
     product
+  })
+}
+
+// Get product by ID ==> /api/v1/products/:id
+export const getProduct = async (req, res) => {
+  const product = await Product.findById(req?.params?.id)
+
+  if (!product)
+    {
+      return res.status(404).json({
+        error: 'Product not found'
+      })
+    }
+  res.status(200).json({
+    product
+  })
+}
+
+// Update product by ID ==> /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id)
+
+  if (!product)
+    {
+      return res.status(404).json({
+        error: 'Product not found'
+      })
+    }
+  
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { new: true} )
+  res.status(200).json({
+    product
+  })
+}
+
+// Delete product by ID ==> /api/v1/products/:id
+export const deleteProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id)
+
+  if (!product)
+    {
+      return res.status(404).json({
+        error: 'Product not found'
+      })
+    }
+  
+  await Product.deleteOne()
+  res.status(200).json({
+    message: 'Product deleted'
   })
 }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { getProducts, newProduct } from '../controllers/productControllers.js'
+import { deleteProduct, getProduct, getProducts, newProduct, updateProduct } from '../controllers/productControllers.js'
 
 const router = express.Router()
 
 router.route("/products").get(getProducts)
 router.route("/admin/products").post(newProduct)
+router.route("/products/:id").get(getProduct)
+router.route("/products/:id").put(updateProduct)
+router.route("/products/:id").delete(deleteProduct)
+
 
 
 export default router;
