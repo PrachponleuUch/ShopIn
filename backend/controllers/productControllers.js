db150f9943d7341f3c7c86e9fd22a8b3cf79ea4f
PrachponleuUch <ponleuvuthy@gmail.com>
2024-07-01 11:44:01 +1000

    Added filter by NAME for products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,11 +1,15 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js"
 import Product from "../models/products.js"
+import APIFilters from "../utils/apiFilters.js"
 import ErrorHandler from "../utils/errorHandler.js"
 
 // Get all products ==> /api/v1/products
 export const getProducts = catchAsyncErrors( async (req, res) => {
-  const products = await Product.find()
+  const apiFilters = new APIFilters(Product, req.query).search()
+  let products = await apiFilters.query
+  let filteredProductsCount = products.length
   res.status(200).json({
+    filteredProductsCount,
     products
   })
 })
