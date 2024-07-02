d5ee11413b7764ff8958fc600ca676c878b0423f
PrachponleuUch <ponleuvuthy@gmail.com>
2024-07-02 15:11:23 +1000

    Added authorization and roles that can access product routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import { deleteProduct, getProduct, getProducts, newProduct, updateProduct } from '../controllers/productControllers.js'
-import { isAuthenticatedUser } from '../middlewares/auth.js'
+import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.route("/products").get(isAuthenticatedUser ,getProducts)
-router.route("/admin/products").post(newProduct)
+router.route("/products").get(getProducts)
+router.route("/admin/products").post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
 router.route("/products/:id").get(getProduct)
-router.route("/products/:id").put(updateProduct)
-router.route("/products/:id").delete(deleteProduct)
+router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+router.route("/admin/products/:id").delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
 
 
 
